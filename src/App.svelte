058b05ed5f4bbf9dcdce3d3bf2ba410adb80c0f9
thepiguy <kunal.a.dandekar@gmail.com>
2022-12-09 20:26:06 +0100

    stuff

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { Client } from "tcore.js";
+
+  const tcore = new Client();
+
+  let name = "";
+
+  async function getName() {
+    await tcore.InitRest();
+
+    const res = await tcore.sendRequest({
+      hook: "name",
+      function: "read_name",
+      action: "null",
+      params: [],
+    });
+
+    const data = await res.json();
+
+    name = data.data.msg;
+  }
+
+  getName();
+</script>
+
+<main>
+  <h1>
+    Hello <span class="gradient">{name}</span>
+  </h1>
+</main>
+
+<style>
+  span.gradient {
+    background: -webkit-linear-gradient(rgb(183, 61, 61), rgb(12, 76, 153));
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+</style>
